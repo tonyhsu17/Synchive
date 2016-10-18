d1846eadbbac6c0d5acef50a39a7682c853623e5
Tony <nuttynuts7@gmail.com>
2016-10-17 21:25:34 -0700

    fix

--- test/DestFileProcJUnitTest.java
+++ test/DestFileProcJUnitTest.java
@@ -30,7 +30,6 @@ public class DestFileProcJUnitTest
     public TemporaryFolder folder = new TemporaryFolder();
     private DestinationFileProcessor destFP;
     private File idFile;
-    private boolean shouldTest;
     
     /**
      * @throws java.lang.Exception
@@ -56,31 +55,20 @@ public class DestFileProcJUnitTest
     @Before
     public void setUp() throws Exception
     {
-        shouldTest = true;
         setUpDirectory();
-        destFP = new DestinationFileProcessor(folder.getRoot());
     }
     
-    private void setUpDirectory()
+    private void setUpDirectory() throws IOException
     {
-        try
-        {
-            idFile = folder.newFile(Utilities.ID_FILE_NAME);
-            FileWriter writer = new FileWriter(idFile);
-            writer.write("Synchive v1.1 - root=D:\\TestA\n");
-            writer.write("~0: \n");
-            writer.write("00000000 \"file1\"\n");
-            writer.write("5AD84AD3 \"file2\"\n");
-            writer.write("~1: \\Test\n");
-            writer.write("70c4251b \"HIHI\"");
-            writer.close(); 
-        }
-        catch(IOException e)
-        {
-            System.out.println("No write permission, skipping tests");
-            shouldTest = false;
-        }
-        
+         idFile = folder.newFile(Utilities.ID_FILE_NAME);
+         FileWriter writer = new FileWriter(idFile);
+         writer.write("Synchive v1.1 - root=D:\\TestA\n");
+         writer.write("~0: \n");
+         writer.write("00000000 \"file1\"\n");
+         writer.write("5AD84AD3 \"file2\"\n");
+         writer.write("~1: \\Test\n");
+         writer.write("70c4251b \"HIHI\"");
+         writer.close(); 
     }
 
     /**
@@ -95,10 +83,8 @@ public class DestFileProcJUnitTest
     @Test
     public void testDecodedFile() throws IOException
     {
-        if(!shouldTest)
-        {
-            return;
-        }
+        destFP = new DestinationFileProcessor(folder.getRoot());
+        
         Hashtable<String, SynchiveDirectory> table = destFP.getFiles();
         assertEquals(2, table.size());
         
@@ -120,12 +106,7 @@ public class DestFileProcJUnitTest
     
     @Test
     public void testReadFiles() throws Exception
-    {
-        if(!shouldTest)
-        {
-            return;
-        }
-        
+    {        
         if(idFile != null)
         {
             idFile.delete();
